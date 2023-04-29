9af846a60d5653d7c3b97d2ac7cc74b6d679b2ee
Alexander Melnikov <melnikov150990@mail.ru>
2023-04-29 22:53:16 +0500

    request, response from api

--- json-lib/src/main/java/ru/asmelnikov/json/domain/protocol/JsonRpcRequest.kt
+++ json-lib/src/main/java/ru/asmelnikov/json/domain/protocol/JsonRpcRequest.kt
@@ -1,8 +1,8 @@
 package ru.asmelnikov.json.domain.protocol
 
 data class JsonRpcRequest(
+    val jsonrpc: String = "2.0",
     val id: Long,
     val method: String,
     val params: Map<String, Any?> = emptyMap(),
-    val jsonRpc: String = "2.0"
 )
